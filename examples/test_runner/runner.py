e6ae647a4e5401b2dd58c9c4a1fea802fee2c214
frozenpine <sonny.frozenpine@gmail.com>
2019-09-02 11:51:22 +0800

    完成请求回报同步，并通过正常路径测试

--- examples/test_runner/runner.py
+++ examples/test_runner/runner.py
@@ -5,7 +5,7 @@ import re
 import json
 
 from pprint import pprint
-from collections import OrderedDict, namedtuple, ChainMap
+from collections import OrderedDict, namedtuple, ChainMap, UserDict
 from threading import RLock, Condition
 from functools import wraps
 from bravado.client import ResourceDecorator, CallableOperation
@@ -30,7 +30,6 @@ class DataMixin(object):
     __TABLE_PATTERN = re.compile(r'#.+#')
 
     def link(self, result_data):
-
         self.__REC_DATA_MAPPER[self] = result_data
 
     def last_order(self):
@@ -113,60 +112,114 @@ class DataMixin(object):
         return data_dict
 
 
-def notify_rtn(func):
-    @wraps(func)
-    def waiter(self, *args, **kwargs):
-        result = func(self, *args, **kwargs)
+def notify_rtn(wait_flag, wait_condition):
+    def waiter(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            result = func(self, *args, **kwargs)
 
-        if self.sync_request_rtn:
-            with self._wait_condition:
-                self._wait_condition.notify()
+            if getattr(self, wait_flag):
+                wait = getattr(self, wait_condition)
+                with wait:
+                    wait.notify()
 
-        return result
+            return result
+
+        return wrapper
 
     return waiter
 
 
+class RequestCache(UserDict):
+    def __init__(self):
+        super(RequestCache, self).__init__()
+
+        self._inflight_cache = OrderedDict()
+        self._request_cache = OrderedDict()
+
+        self._cache = ChainMap(self._inflight_cache, self._request_cache)
+
+    def is_inflight(self, key):
+        return key in self._inflight_cache
+
+    def inflight(self, key, value):
+        self._request_cache[key] = self._inflight_cache[key] = value
+
+    def __setitem__(self, key, value):
+        if key not in self._inflight_cache and key not in self._request_cache:
+            return self.inflight(key, value)
+
+        if key in self._inflight_cache:
+            self._inflight_cache.pop(key)
+
+        self._request_cache[key] = value
+
+    def __getitem__(self, item):
+        return self._cache[item]
+
+    def __getattr__(self, item):
+        return self._cache[item]
+
+    def __delitem__(self, key):
+        if key in self._inflight_cache:
+            del self._inflight_cache[key]
+
+        del self._request_cache[key]
+
+    def __len__(self):
+        return len(self._cache)
+
+    def __contains__(self, item):
+        return item in self._cache
+
+    def __iter__(self):
+        return self._cache.__iter__()
+
+
 class APITester(Trader):
-    sync_request_rtn = True
-    sync_rtn_wait = 10
+    SYNC_REQ_WITH_RTN = True
+    RTN_WAIT_TIMEOUT = 10
 
     def __init__(self, host="https://www.btcmex.com",
                  symbol="XBTUSD", api_key="", api_secret=""):
-        self._request_orders = OrderedDict()
+        self._request_cache = RequestCache()
 
-        self._rtn_orders = OrderedDict()
-        self._rtn_trade = OrderedDict()
+        self._rtn_order_cache = OrderedDict()
+        self._rtn_trade_cache = OrderedDict()
 
-        self._rtn_caches = ChainMap(self._rtn_orders, self._rtn_trade)
+        self._rtn_caches = ChainMap(self._rtn_order_cache,
+                                    self._rtn_trade_cache)
 
         self._wait_condition = Condition(RLock())
 
         super(APITester, self).__init__(host=host, symbol=symbol,
                                         api_key=api_key,
                                         api_secret=api_secret)
+        self.logger = logging.getLogger(__file__)
 
-    @notify_rtn
+    @notify_rtn(wait_flag="SYNC_REQ_WITH_RTN",
+                wait_condition="_wait_condition")
     def on_rtn_order(self, order_data: dict, ts: int = None):
         order_id = order_data["orderID"]
 
-        if order_id not in self._request_orders:
+        if order_id not in self._request_cache:
             return
 
-        self._rtn_orders[order_id] = (order_data, ts)
+        self._rtn_order_cache[order_id] = (order_data, ts)
 
         print("委托回报:")
         pprint(order_data)
         print()
 
-    @notify_rtn
+    @notify_rtn(wait_flag="SYNC_REQ_WITH_RTN",
+                wait_condition="_wait_condition")
     def on_rtn_trade(self, trade_data: dict, ts: int = None):
         order_id = trade_data["orderID"]
 
-        if order_id not in self._request_orders:
+        if order_id not in self._request_cache:
             return
 
-        self._rtn_trade[order_id] = (trade_data, ts)
+        self._rtn_trade_cache[order_id] = (trade_data, ts)
 
         print("成交回报:")
         pprint(trade_data)
@@ -174,29 +227,18 @@ class APITester(Trader):
 
     def __getattr__(self, item):
         class ResourceWrapper:
-            __args_tuple = namedtuple(
-                "args_tuple", ("logger", "req_order_cache", "sync_request_rtn",
-                               "wait_condition", "sync_rtn_wait",
-                               "rtn_order_cache"))
+            args_tuple = namedtuple(
+                "args_tuple", ("sync_req_rtn", "wait_condition",
+                               "rtn_wait_timeout"))
 
-            def __init__(self, constraints, origin_res, args):
-                self.key_name, self.constraints = constraints
-                self.args = ResourceWrapper.__args_tuple(*args)
+            def __init__(self, req_key: str, req_cache: RequestCache,
+                         rtn_cache, logger, origin_res, args: args_tuple):
+                self.key_name = req_key
+                self.req_cache = req_cache
+                self.rtn_cache = rtn_cache
+                self.logger = logger
+                self.args = args
                 self.origin_resource = origin_res
-
-            def __check_constraint(self, req_data):
-                key_value = req_data[self.key_name]
-
-                if key_value not in self.args.rtn_order_cache:
-                    return False
-
-                for constraint_name in self.constraints:
-                    constraint_value = req_data[constraint_name]
-                    rtn_order, _ = self.args.rtn_order_cache[key_value]
-                    if rtn_order[constraint_name] != constraint_value:
-                        return False
-
-                return True
 
             def __call__(self, *args, **kwargs):
                 if isinstance(self.origin_resource, CallableOperation):
@@ -204,11 +246,11 @@ class APITester(Trader):
 
                     try:
                         req_ts = time_ms()
-                        req_results, _ = http_future.result()
-                        self.args.logger.info("发送请求 {} : {} {}".format(
+                        req_results, rsp = http_future.result()
+                        self.logger.info("发送请求 {} : {} {}".format(
                             self.origin_resource.operation.operation_id,
                             args if args else "", kwargs if kwargs else ""))
-                        self.args.logger.debug(
+                        self.logger.debug(
                             "url: {}, method: {}, header: {}, data: {}".format(
                                 http_future.future.request.url,
                                 http_future.future.request.method,
@@ -216,39 +258,38 @@ class APITester(Trader):
                                 http_future.future.request.json
                             ))
                     except Exception as e:
-                        self.args.logger.exception(e)
-                    else:
-                        if not isinstance(req_results, list):
-                            req_results = [req_results]
+                        self.logger.exception(e)
+                        return [e]
 
-                        rtn_results = list()
+                    if not isinstance(req_results, list):
+                        req_results = [req_results]
 
-                        for result in req_results:
-                            key_value = result[self.key_name]
+                    if not self.args.sync_req_rtn:
+                        return req_results
 
-                            self.args.req_order_cache[key_value] = result
+                    rtn_results = list()
 
-                            if self.args.sync_request_rtn:
-                                with self.args.wait_condition:
-                                    if not self.__check_constraint(result):
-                                        self.args.wait_condition.wait(
-                                            self.args.sync_rtn_wait)
+                    for result in req_results:
+                        key_value = result[self.key_name]
 
-                            try:
-                                rtn_result, rtn_ts = self.args.rtn_order_cache[
-                                    key_value]
-                            except KeyError:
-                                pass
-                            else:
-                                rtn_results.append(rtn_result)
+                        self.req_cache.inflight(key_value, result)
 
-                                self.args.logger.info(
-                                    "request[{}] round robin: {} ms".format(
-                                        rtn_result,
-                                        rtn_ts - req_ts))
+                        with self.args.wait_condition:
+                            if self.req_cache.is_inflight(key_value):
+                                self.args.wait_condition.wait(
+                                    self.args.rtn_wait_timeout)
 
-                        return (rtn_results if self.args.sync_request_rtn else
-                                req_results)
+                        rtn_result, rtn_ts = self.rtn_cache[key_value]
+                        rtn_results.append(rtn_result)
+                        self.req_cache[key_value] = rtn_result
+
+                        if rtn_ts:
+                            self.logger.info(
+                                "request[{}] round robin: {} ms".format(
+                                    rtn_result,
+                                    rtn_ts - req_ts))
+
+                    return rtn_results
 
                 return self.origin_resource(*args, **kwargs)
 
@@ -262,18 +303,25 @@ class APITester(Trader):
 
                 return origin_attr
 
-        # todo: 寻找一个 委托 <--> 回报 的唯一性约束，当前约束在特定条件下无效
-        constraint_mapper = {
-            "Order": ("orderID", ("orderQty", ))
+        key_mapper = {
+            "Order": "orderID"
+        }
+        rtn_cache_mapper = {
+            "Order": self._rtn_order_cache
         }
 
         origin_attribute = getattr(self._rest_client, item)
 
         if isinstance(origin_attribute, ResourceDecorator):
+            # noinspection PyCallByClass
             return ResourceWrapper(
-                constraint_mapper[item], origin_attribute,
-                (self.logger, self._request_orders, self.sync_request_rtn,
-                 self._wait_condition, self.sync_rtn_wait, self._rtn_orders))
+                req_key=key_mapper[item], req_cache=self._request_cache,
+                rtn_cache=rtn_cache_mapper[item], logger=self.logger,
+                origin_res=origin_attribute,
+                args=ResourceWrapper.args_tuple(
+                    sync_req_rtn=self.SYNC_REQ_WITH_RTN,
+                    wait_condition=self._wait_condition,
+                    rtn_wait_timeout=self.RTN_WAIT_TIMEOUT))
 
         return origin_attribute
 
@@ -297,8 +345,6 @@ if __name__ == "__main__":
 
     for order in order_list:
         data = order.data()
-        # 该 "text" 自定义字段在改单时，会被 clear 修改为“改量”还是“改价”状态
-        data.update({"text": "test"})
         resource = getattr(ex, resource_name)
         for order_result in getattr(resource, order.action.strip())(**data):
             order.link(order_result)
